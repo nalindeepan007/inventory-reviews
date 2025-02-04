06c6384fa2b020fcac7073bb83bbc75df7453eb5
Nalin Deepan <nalindeepan007@gmail.com>
2025-02-04 16:39:21 +0530

    pydantic schema for review response api

--- app/schemas.py
+++ app/schemas.py
@@ -3,11 +3,27 @@ from datetime import datetime
 from typing import Optional
 
 
-class CategoryTrend(BaseModel):
+class CategoryReviewTrend(BaseModel):
     id: int
     name: str
     description: Optional[str] = None
     average_stars: float
     total_reviews: int
+    class Config:
+        from_attributes = True
+
+
+class ReviewBase(BaseModel):
+    text: Optional[str]
+    stars: int
+    review_id: str
+    tone: Optional[str]
+    sentiment: Optional[str]
+    category_id: int
+
+class ReviewResponse(ReviewBase):
+    id: int
+    created_at: datetime
+    
     class Config:
         from_attributes = True
